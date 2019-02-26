8ecc92948152d868ad9f4d9e171b1c671210500a
su.dan <su.dandan@dian-tong.com>
2019-02-26 09:04:23 +0800

    update code

--- src/main/java/com/wsh/service/userService.java
+++
@@ -1,8 +0,0 @@
-package com.wsh.service;
-import net.sf.json.JSONObject;
-
-
-public interface userService {
-    /*Map<String, String> saveUser(JSONObject jsonObject);*/
-    JSONObject loginUser(JSONObject jsonObject) throws Exception;
-}
--- src/main/java/com/wsh/servlet/IsChOrEnOrNum.java
+++ src/main/java/com/wsh/servlet/IsChOrEnOrNum.java
@@ -6,21 +6,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class IsChOrEnOrNum {
-    @Test
+
     public static String  IsChOrNum(String content){
         String keyType = "";
-        String regex = "^[\\u4e00-\\u9fa5_a-zA-Z]*$";
+        String regex = "^[\\u4e00-\\u9fa5]*$";
         Pattern pattern = Pattern.compile(regex);
         Matcher match = pattern.matcher(content);
         boolean b = match.matches();
-        if (b == true) {
+        if (b) {
             keyType = "chinese";
         } else {
             regex = "^[0-9]*$";
             pattern = Pattern.compile(regex);
             match = pattern.matcher(content);
             b = match.matches();
-            if (b==true) {
+            if (b) {
                 keyType = "number";
             } else {
                 keyType = "english";
@@ -28,4 +28,5 @@ public class IsChOrEnOrNum {
         }
         return keyType;
     }
+
 }
