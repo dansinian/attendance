2a94d860d179fae94da21d4c1737eb50fdb07241
窦超 <302548234@qq.com>
2019-04-07 10:55:15 +0800

    Update QuestionController.java

--- src/main/java/com/wsh/controller/QuestionController.java
+++ src/main/java/com/wsh/controller/QuestionController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
 
 @Controller
 @RequestMapping("/question")
@@ -62,7 +63,7 @@ public class QuestionController {
 
     @RequestMapping("/recommendQuestion")
     @ResponseBody
-    public JSONObject recommendQuestion(HttpServletRequest request){
+    public JSONObject recommendQuestion(HttpServletRequest request) throws ParseException {
         return questionService.recommendQuestion();
     }
 
